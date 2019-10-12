0457539c701068215f95f08b51543a9078ccabee
younes <younes@interactive-object.com>
2019-10-12 17:00:49 +0100

    fix toe_type

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const vue = require('./lib/index')
 
 // console.log(vue.attributes)
-console.log(vue.attributesByCategory("tops"))
-// console.log(vue.attributeValues("bags", "color"))
+// console.log(vue.attributesByCategory("tops"))
+console.log(vue.attributeValues("footwear", "toe_type"))
 
